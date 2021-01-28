498698be24d3af5aa6b0a4058dd33afc4d73e206
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-28 12:45:14 -0600

    learned how to chain promises

--- script.js
+++ script.js
@@ -103,7 +103,18 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json()) // First convert response to json
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('mexico');
